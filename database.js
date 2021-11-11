64d0fd0f0a1769652eb047c6a60cce92942c3d12
Solai-Adithya <solaiadithya001@gmail.com>
2021-11-11 17:33:34 +0530

    updated schema and added API

--- database.js
+++ database.js
@@ -4,18 +4,21 @@ const { createClient } = require('@supabase/supabase-js')
 const supabase = createClient(process.env.DB_URL, process.env.DB_KEY)
 
 class API {
-
   async newParticipant(participant) {
-    const facelistID = 1; //later randomise it or alter according to business logic.
-    const { data, error } = await supabase.from('Participants').insert({age: participant.age, facelistID: facelistID});
+    // for example participant should be an object like {age:19, handedness:'left', ...}
+    const { data, error } = await supabase.from('Participants').insert(participant);
     const participantID = data[0].participantID;
     return participantID;
   }
 
   async updateScore(participantID, param, score) {
-    const { data, error } = await supabase.from('Participants').where('participantID', participantID).update({[param]: score});
+    //If row is not present, it will be inserted. If present, row will be updated.
+    const { data, error } = await supabase.from('Score').upsert({participantID: participantID, [param]: score});
   }
 
+  async addNewFace(imageID, name, affiliation) {
+    const {data, error} = await supabase.from('Facedata').insert({imageID: imageID, name: name, affiliation: affiliation});
+  }
 }
 
 module.exports = API;
