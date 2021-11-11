7cdb7d351119c44e1e099f45caea49bee0720536
harnaman-hk <harnaman.hk@gmail.com>
2021-11-12 03:00:52 +0530

    display images periodically

--- database.js
+++ database.js
@@ -4,28 +4,40 @@ const { createClient } = require('@supabase/supabase-js')
 const supabase = createClient(process.env.DB_URL, process.env.DB_KEY)
 
 class API {
-  async newParticipant(participant) {
-    const { data, error } = await supabase.from('Participants').insert(participant);
-    console.log("New Participant DB Query Data: ", data, " Error: ", error)
-    const participantID = data[0].participantID;
-    const scoreData = await this.addEmptyScoreEntry(participantID);
-    return participantID;
-  }
+    async newParticipant(participant) {
+        const { data, error } = await supabase.from('Participants').insert(participant);
+        console.log("New Participant DB Query Data: ", data, " Error: ", error)
+        const participantID = data[0].participantID;
+        const scoreData = await this.addEmptyScoreEntry(participantID);
+        return participantID;
+    }
 
-  async addEmptyScoreEntry(participantID, facelistID=1) {
-    const { data, error } = await supabase.from('Score').insert({participantID: participantID, facelistID: facelistID});
-    return data;
-  }
+    async addEmptyScoreEntry(participantID, facelistID = 1) {
+        const { data, error } = await supabase.from('Score').insert({ participantID: participantID, facelistID: facelistID });
+        return data;
+    }
 
-  async updateScore(participantID, param, score) {
-    const { data, error } = await supabase.from('Score').where({participantID: participantID}).update({[param]: score});
-    return data;
-  }
+    async updateScore(participantID, param, score) {
+        const { data, error } = await supabase.from('Score').where({ participantID: participantID }).update({
+            [param]: score
+        });
+        return data;
+    }
 
-  async addNewFace(imageID, name, affiliation) {
-    const {data, error} = await supabase.from('Facedata').insert({imageID: imageID, name: name, affiliation: affiliation});
-    return data;
-  }
+    async addNewFace(imageID, name, affiliation) {
+        const { data, error } = await supabase.from('Facedata').insert({ imageID: imageID, name: name, affiliation: affiliation });
+        return data;
+    }
+
+    async fetchData() {
+        const { data, error } = await supabase
+            .from('Facedata')
+            .select();
+        if (error) {
+            throw (error);
+        }
+        return data;
+    }
 }
 
 module.exports = API;
--- server.js
+++ server.js
@@ -4,49 +4,48 @@ var API = require('./database')
 const db = new API()
 var app = express()
 var bodyParser = require("body-parser")
-var participantID
+var participantID, facenameData;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(require("express-session")({
-  secret: "Rusty is a dog",
-  resave: false,
-  saveUninitialized: false
+    secret: "Rusty is a dog",
+    resave: false,
+    saveUninitialized: false
 }));
-
 app.use(express.static(path.join(__dirname, '/public')));
+init();
 
 app.use('/corsiblocktapping', require('./corsiblock'));
+// app.use('/facename', require('./facename'));
 
-app.get('/face-name', function(req, res) {
-  // console.log("inside main server get route face-name\n")
-  res.sendFile('facename/facename.html', { root: './public/'});
+app.get('/face-name', async function(req, res) {
+    // console.log("inside main server get route face-name\n")
+    res.render('facename.ejs', { data: await db.fetchData(), txt: "helloooo" });
 });
 
 
 app.get('/', function(req, res) {
-  res.render("input_page")
+    res.render("input_page")
 });
-app.post("/", async function (req, res) {
-  const formdata = req.body
-  participantID = await db.newParticipant(formdata);
-  console.log("Participant ID is: ", participantID);
-  res.render("main_page")
+app.post("/", async function(req, res) {
+    const formdata = req.body
+    participantID = await db.newParticipant(formdata);
+    console.log("Participant ID is: ", participantID);
+    res.render("main_page")
 });
 
 app.get('/api', (req, res) => {
-  res.json({ message: "Hello from server!" });
+    res.json({ message: "Hello from server!" });
 });
-
-
 
 
 // Showing names page recalled in last
-app.get("/names", function (req, res) {
+app.get("/names", function(req, res) {
     res.render("names");
 });
 // handling the req 
-app.post("/names", function (req, res) {
+app.post("/names", function(req, res) {
     const x = req.body
     console.log("\nNames recalled :-\n")
     console.log(x.names)
@@ -55,11 +54,12 @@ app.post("/names", function (req, res) {
 
 
 // Showing occupations page recalled in last
-app.get("/occupations", function (req, res) {
+app.get("/occupations", function(req, res) {
     res.render("occupations");
 });
+
 // handling the req 
-app.post("/occupations", function (req, res) {
+app.post("/occupations", function(req, res) {
     const x = req.body
     console.log("\nOccupation recalled :-\n")
     console.log(x.occupations)
@@ -79,3 +79,7 @@ app.post("/occupations", function (req, res) {
 var port = Number(process.env.PORT || 8080);
 app.listen(port);
 console.log("App running at port", port);
+
+function init() {
+    facenameData = db.fetchData();
+}
